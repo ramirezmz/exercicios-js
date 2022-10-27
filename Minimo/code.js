3393d0d4748c8c69e4166fad88a985e4b047efc6
ramirezmz <robertopaoloramirezmunoz7@gmail.com>
2022-10-27 16:47:58 -0300

    minimum completed

--- Minimo/code.js
+++ Minimo/code.js
@@ -1,5 +1,13 @@
-function minimo() {
-    // code
+function minimo(x, y) {
+  if(x === y){
+    console.log(`${x} e ${y} são iguais`)
+  } else{
+    if(x > y) {
+      return y
+    } else {
+      return x
+    }
+  }
 }
 
 module.exports = minimo
