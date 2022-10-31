86b23dfb64075b4e0c7b7ed1a482c41ccd60c18b
ramirezmz <robertopaoloramirezmunoz7@gmail.com>
2022-10-31 16:56:24 -0300

    contando feijoes completed

--- Contando Feijões/code.js
+++ Contando Feijões/code.js
@@ -1,11 +1,36 @@
 function countBs(word) {
-    // code
+    let countB = 0
+    for(let i = 0; i < word.length; i++) {
+       if(word.charAt(i) != 'B') {
+        countB += 0
+            // console.log(word.charAt(i), 'false')
+       } else {
+        // console.log(word.charAt(i), 'true')
+        countB ++
+       }
+    }
+    return countB
 }
 
 function countChar(word, char) {
-    // code
+    let countBs = 0
+    let lengthWord = word.length
+
+    for( let i = 0; i < lengthWord ; i++) {
+        if(word.charAt(i) != char) {
+            countBs += 0
+        } else {
+            countBs ++
+        }
+    }
+    return countBs
 }
 
+console.log(countBs('BBC'))
+console.log(countBs("Bruno"))
+console.log(countBs("bruno"))
+console.log(countChar("kakkerlak", "k"))
+console.log(countChar("larissa", "a"))
 module.exports = {
     countBs,
     countChar
