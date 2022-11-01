4b3322f15a719690519fba3e6bc9059523f25eea
ramirezmz <robertopaoloramirezmunoz7@gmail.com>
2022-11-01 15:26:18 -0300

    Meu objeto completed

--- Meu Objeto/code.js
+++ Meu Objeto/code.js
@@ -1,5 +1,18 @@
 function code(meuObjeto) {
-    // code
+    const keys = Object.keys(meuObjeto) 
+    for(let i = 0; i < keys.length ; i++) {
+        let indexKey = keys[i]
+        console.log(`Meu ${keys[i]} é ${meuObjeto[indexKey]}`)
+    }
 }
+
+const eu = {
+    nome: 'Roberto',
+    cabelo: 'Preto',
+    monitor: 'Pequeno',
+    copo: 'Branco'
+}
+
+// code(eu)
 
 module.exports = code
