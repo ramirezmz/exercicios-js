b45ef8d9de013ec5df7905756a3c9838c792319e
ramirezmz <robertopaoloramirezmunoz7@gmail.com>
2022-11-07 17:48:23 -0300

    refactor meu objeto

--- Meu Objeto/code.js
+++ Meu Objeto/code.js
@@ -1,10 +1,29 @@
 function code(meuObjeto) {
     const keys = Object.keys(meuObjeto) 
+
     for(let i = 0; i < keys.length ; i++) {
         let indexKey = keys[i]
         console.log(`Meu ${keys[i]} é ${meuObjeto[indexKey]}`)
     }
+
+    for(var key of keys) {
+        console.log(`Meu ${keys[key]} é ${meuObjeto[key]}`)
+    }
+
+    for(var property in meuObjeto) {
+        console.log(`Meu ${property} é ${meuObjeto[property]}`)
+    }
+
+    for(let [key, value] of Object.entries(meuObjeto)) {
+        console.log(`Meu ${key} é ${value}`)
+    }
 }
+
+/**
+ * keys => retorna as propriedades
+ * entries => retorna propriedade e valor
+ * values => retorna só valores
+ */
 
 const eu = {
     nome: 'Roberto',
@@ -13,6 +32,6 @@ const eu = {
     copo: 'Branco'
 }
 
-// code(eu)
+code(eu)
 
 module.exports = code
