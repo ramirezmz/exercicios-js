62ba924666721755f8bc749858d9621424172c19
ramirezmz <robertopaoloramirezmunoz7@gmail.com>
2022-11-07 17:47:42 -0300

    calculadora completed

--- Calculadora/code.js
+++ Calculadora/code.js
@@ -1,3 +1,23 @@
 // define your class here
 
+class Calculadora {
+
+  somar(firstNumber, secondNumber) {
+    return firstNumber + secondNumber
+  }
+
+  subtrair(firstNumber, secondNumber) {
+    return firstNumber - secondNumber
+  }
+
+  multiplicar(firstNumber, secondNumber) {
+    return firstNumber * secondNumber
+  }
+
+  dividir(firstNumber, secondNumber) {
+    return firstNumber / secondNumber
+  }
+}
+
+
 module.exports = Calculadora
